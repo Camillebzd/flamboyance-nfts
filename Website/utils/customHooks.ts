b0463e0923924e403bd129ca55dbf992e2e6f985
CamilleBzd <bcamille99@gmail.com>
2023-10-13 18:56:06 +0200

    [FIX] ReferenceError: window is not defined

--- Website/utils/customHooks.ts
+++ Website/utils/customHooks.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  if (window === undefined) {
+  if (typeof window === "undefined") {
     return {width: 0, height: 0};
   }
   const { innerWidth: width, innerHeight: height } = window;
